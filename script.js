7ea166740ab91414aa3db027d17819a9028abe03
ninja6734 <fortnite.skurka@web.de>
2024-04-18 17:16:29 +0200

    update

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ function MainButtons(target){
     const targetElement = document.getElementById(target);
     targetElement.scrollIntoView({behavior: "smooth"})
 }
+function OpenWebsite(){
+    window.open("HauptFile_4.4.c");
+}
