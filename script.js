6f0731ea987ed680c9454f1ecb0f5bb1a85410a0
ninja6734 <fortnite.skurka@web.de>
2024-04-26 15:38:09 +0200

    commit

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ function MainButtons(target){
 function OpenWebsite(){
     window.open("HauptFile_4.4.c");
 }
+function ChangeTheme(){
+    var rootElement = document.querySelector(":root");
+    rootElement.style.setProperty("--light_button","#75858b");
+}
+
+function LoadTheme(){
+
+}
+
+window.onload = LoadTheme();
